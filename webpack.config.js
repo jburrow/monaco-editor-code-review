5b00d5ff08c7fa9a6a792f3da7338f4b23801b2c
jburrow <jaburrow@gmail.com>
2019-05-15 09:48:10 +0100

    Add timestamp

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const TimestampWebpackPlugin = require("timestamp-webpack-plugin");
 
 module.exports = {
   entry: { index: "./src/index.ts" },
@@ -24,5 +25,11 @@ module.exports = {
     path: path.join(__dirname, "dist"),
     libraryTarget: "var",
     library: "MonacoEditorCodeReview"
-  }
+  },
+  plugins: [
+    new TimestampWebpackPlugin({
+      path: path.join(__dirname, "dist"),
+      filename: "timestamp.json"
+    })
+  ]
 };
